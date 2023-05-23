4e96d4cd6fe60cff4e6a383e78316b9fc2351e04
Vitaly Akindinov <vitaly@mail.maxxle.com>
2023-05-24 03:26:21 +0400

    add domain event mediator

---
+++ src/Services/Widgets/Widgets.Application/SeedWork/Mediator/DomainEventMediator.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Concurrent;
+using System.Reflection;
+
+namespace Widgets.Application.SeedWork.Mediator;
+
+public sealed class DomainEventMediator : IDomainEventMediator
+{
+    private readonly ConcurrentDictionary<Type, List<object>> _handlers = new();
+
+    private readonly IServiceScopeFactory _serviceProviderFactory;
+    private bool _isInitialized = false;
+
+    public DomainEventMediator(IServiceScopeFactory serviceScopeFactory)
+    {
+        _serviceProviderFactory = serviceScopeFactory;
+    }
+
+    public async Task Publish<T>(T domainEvent, CancellationToken cancellationToken = default)
+        where T : IDomainEvent
+    {
+        var domainEventType = domainEvent.GetType();
+        if (_handlers.ContainsKey(domainEventType))
+            foreach (var handler in _handlers[domainEventType])
+            {
+                var handlerType = handler.GetType();
+                var domainHandler = Convert.ChangeType(handler, handlerType);
+
+                var handleMethod = handler
+                    .GetType()
+                    .GetMethod(nameof(IDomainEventHandler<IDomainEvent>.Handle));
+
+                var asyncMethod = (Task)handleMethod!.Invoke(handler, new object[] { domainEvent, cancellationToken })!;
+
+                await asyncMethod;
+            }
+    }
+
+    public void Register<T>(IDomainEventHandler<T> handler)
+        where T : IDomainEvent
+    {
+        var domainEventType = typeof(T);
+        if (!_handlers.ContainsKey(domainEventType))
+            _handlers[domainEventType] = new();
+
+        _handlers[domainEventType].Add(handler);
+    }
+
+    public void Initialize()
+    {
+        if (_isInitialized)
+            throw new InvalidOperationException($"{typeof(DomainEventMediator).FullName} was already initialized");
+
+        _isInitialized = true;
+
+        var domainEventHandlerTypes = Assembly
+                .GetAssembly(typeof(DomainEventMediator))!
+                .GetTypes()
+                .Where(x => x.GetInterfaces()
+                    .Any(y => y.IsGenericType
+                           && y.GetGenericTypeDefinition() == typeof(IDomainEventHandler<>)));
+
+        using var scope = _serviceProviderFactory.CreateScope();
+        foreach (var handlerType in domainEventHandlerTypes)
+        {
+            var domainEventType = handlerType
+                .GetInterfaces()
+                .First()
+                .GetGenericArguments()
+                .First();
+            var registereMethod = typeof(DomainEventMediator)
+                .GetMethod(nameof(DomainEventMediator.Register))!
+                .MakeGenericMethod(domainEventType!);
+
+            var handlerInstance = ActivatorUtilities.CreateInstance(scope.ServiceProvider, handlerType)!;
+
+            registereMethod.Invoke(this, new object[] { handlerInstance });
+        }
+    }
+}
---
+++ src/Services/Widgets/Widgets.Application/SeedWork/Mediator/IDomainEventMediator.cs
@@ -0,0 +1,8 @@
+﻿namespace Widgets.Application.SeedWork.Mediator;
+
+public interface IDomainEventMediator
+{
+    //Task DispatchDomainEventsAsync(IUserDbContext context, CancellationToken cancellationToken = default);
+    void Register<T>(IDomainEventHandler<T> handler) where T : IDomainEvent;
+    Task Publish<T>(T domainEvent, CancellationToken cancellationToken = default) where T : IDomainEvent;
+}
