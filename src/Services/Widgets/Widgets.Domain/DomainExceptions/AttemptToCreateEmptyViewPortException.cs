2d9fccfd92bc528f4e596ac1a0f365f45ced184c
Vitaly Akindinov <vitaly@mail.maxxle.com>
2023-05-24 03:05:26 +0400

    Add project files.

---
+++ src/Services/Widgets/Widgets.Domain/DomainExceptions/AttemptToCreateEmptyViewPortException.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+namespace Subscriptions.Domain.DomainExceptions;
+
+internal class AttemptToCreateEmptyViewPortException
+    : Exception
+{
+    public AttemptToCreateEmptyViewPortException() : base($"Attempt to create an empty viewport") { }
+}
